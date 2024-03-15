342fd45bbc1098ecc0c77da0bcdf1a25238fb9b2
Aswin-Annalath1 <aswinannalath2001@gmail.com>
2024-03-15 10:50:32 +0530

    changes

--- client/src/Components/3_Header.tsx
+++ client/src/Components/3_Header.tsx
@@ -55,7 +55,10 @@ export default function Header() {
   return (
     <header className={darkMode ? "dark-mode" : ""}> {/* Apply dark mode class */}
       <a className="logo">MyBlog</a>
-      <button className="mode" onClick={toggleDarkMode}>{darkMode ? "Light Mode" : "Dark Mode"}</button>
+      {/* <button className="mode" onClick={toggleDarkMode}>{darkMode ? "Light Mode" : "Dark Mode"}</button> */}
+      <button onClick={toggleDarkMode} type="button" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+      {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
       <nav>
         {username && (
           <>
