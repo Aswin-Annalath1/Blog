0436e60766f075547f78a2423f108e674f140dd7
Aswin-Annalath1 <aswinannalath2001@gmail.com>
2024-03-19 12:16:39 +0530

    ch

--- client/src/Components/3_Header.tsx
+++ client/src/Components/3_Header.tsx
@@ -12,6 +12,7 @@ export default function Header() {
   const { userInfo, setUserInfo } = useContext(UserContext);
   const navigate = useNavigate();
   const [darkMode, setDarkMode] = useState(false); // State variable to track dark mode
+  const [isLoading, setIsLoading] = useState(true); // Add loading state
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,6 +33,8 @@ export default function Header() {
       } catch (error) {
         console.error('Error fetching profile:', error);
         // Handle errors as needed
+      }finally {
+        setIsLoading(false); // Set loading state to false regardless of success or failure
       }
     };
     fetchData();
@@ -52,6 +55,9 @@ export default function Header() {
   function toggleDarkMode() {
     setDarkMode(prevMode => !prevMode);
     document.body.classList.toggle("dark-mode");
+  }
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading indicator while fetching user profile
   }
   return (
     <header className={darkMode ? "dark-mode" : ""}> {/* Apply dark mode class */}
